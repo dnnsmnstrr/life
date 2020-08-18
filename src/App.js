f6e95fd5720b6f39609d75ac4439b087e23abe56
Dennis Muensterer <dennismuensterer@gmail.com>
2020-08-18 15:56:41 +0200

    Add life timeline

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import LifeTimeline from './LifeTimeline'
 function App() {
+  const [events, setEvents] = useState([])
+  const handleEvents = async (cb) => {
+    const response = await fetch('https://next.muensterer.xyz/api/events')
+    const json = await response.json()
+    console.log('json', json)
+    setEvents(json)
+  }
+  useEffect(() => {
+    handleEvents()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My life in weeks
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <LifeTimeline
+      events={events}
+      subject={{birthday: new Date('1997-06-16'), name: 'Dennis'}}
+      />
     </div>
   );
 }
