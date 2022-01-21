afd91b6bbed939f49125ab1d1f7c0a7684819116
dnnsmnstrr <dnnsmnstrr@users.noreply.github.com>
2022-01-21 16:13:30 +0000

    deploy: 96dfcdcf01a807694bb47a1abe84df0f124223df

---
+++ precache-manifest.25f7905df384f646f899f68680528b36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a315c196a1393e16a674722ffa66cb",
+    "url": "/life/index.html"
+  },
+  {
+    "revision": "6e850599d47c9e3d32d7",
+    "url": "/life/static/css/main.da6a03f1.chunk.css"
+  },
+  {
+    "revision": "df30666641094ae129b4",
+    "url": "/life/static/js/2.06bfdfe6.chunk.js"
+  },
+  {
+    "revision": "6e850599d47c9e3d32d7",
+    "url": "/life/static/js/main.96801ab9.chunk.js"
+  },
+  {
+    "revision": "2e641e760f18469dcd75",
+    "url": "/life/static/js/runtime~main.b653f72e.js"
+  }
+]);
